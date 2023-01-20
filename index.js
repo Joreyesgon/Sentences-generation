f2ae734987286b01639e6e8dc0ff1fc09d28ebd2
Jose Reyes <jo.reyesgon@gmail.com>
2023-01-19 21:43:42 -0300

    spell check

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const randomSentences = () => {
     let pronoun = ["A", "The"];
     let subject = ["cat", "raccoon", "dog", "rabbit", "bird",];
     let action = ["took my", "threw my", "stole my", "bit my"];
-    let possetion = [ "homework", "toe", "car", "shoe", "pencil"];
+    let possession = [ "homework", "toe", "car", "shoe", "pencil"];
 
     let pronounIndex = Math.floor(Math.random() * pronoun.length);
     let subjectIndex = Math.floor(Math.random() * subject.length);
     let actionIndex = Math.floor(Math.random() * action.length);
-    let possetionIndex = Math.floor(Math.random() * possetion.length);
+    let possessionIndex = Math.floor(Math.random() * possession.length);
     
-    return pronoun[pronounIndex] + " " + subject[subjectIndex] + " " + action[actionIndex] + " " + possetion[possetionIndex] + "!"; 
+    return pronoun[pronounIndex] + " " + subject[subjectIndex] + " " + action[actionIndex] + " " + possession[possessionIndex] + "!"; 
 };
